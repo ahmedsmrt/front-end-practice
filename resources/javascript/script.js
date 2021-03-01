ddf545eef2f06f56d9f7915b153af9a145334f47
A.G.Sudani <36556422+ahmedsmrt@users.noreply.github.com>
2021-02-28 22:40:19 -0500

    Add files via upload

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,1 +1,13 @@
 // JS
+// Testing 
+let cardContTest = document.querySelector('#card-1');
+let cardTitle1 = cardContTest.getElementsByTagName('span');
+const button = document.getElementsByClassName('button-primary')[0];
+
+function cardTitleTest() {
+    let input = prompt('What would you like to study today?');
+    cardTitle1[0].innerHTML = input;
+    console.log(button);
+};
+
+button.addEventListener('click', cardTitleTest);
